3e1a4a81e237b77d84f20bfe4586f129ca294d64
John Duncan <john@johnduncanphoto.com>
2017-09-26 16:46:18 +0100

    added string as int passed 8/2

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -25,3 +25,8 @@ end
 def join_string(string_one, string_two)
   return "#{string_one}#{string_two}"
 end
+
+def add_string_as_number(string_one, string_two)
+  result = string_one.to_i + string_two.to_i
+  return result
+end
