07aca57ab89fdc15ec8e66cfd02e9c3cc6cce934
John Duncan <john@johnduncanphoto.com>
2017-09-26 16:58:07 +0100

    number to month passed 11/1

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -30,3 +30,16 @@ def add_string_as_number(string_one, string_two)
   result = string_one.to_i + string_two.to_i
   return result
 end
+
+def number_to_full_month_name(input_number)
+   month = case input_number
+  when 1
+    "January"
+  when 3
+    "March"
+  when 9
+    "September"
+
+  end
+  return month
+end
